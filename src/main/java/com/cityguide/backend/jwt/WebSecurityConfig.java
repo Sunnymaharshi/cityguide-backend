fbacbe169427dd73b0371d1be33302cff7fa5737
Roy_ Surabhi (surabroy@deloitte.com) <surabroy@your.hostname.com>
2022-05-16 15:53:31 +0530

    updated CORs policy

--- src/main/java/com/cityguide/backend/jwt/WebSecurityConfig.java
+++ src/main/java/com/cityguide/backend/jwt/WebSecurityConfig.java
@@ -79,7 +79,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
