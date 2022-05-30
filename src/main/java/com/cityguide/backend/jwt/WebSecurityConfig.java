4d82c9d3d133ab974a9bdd99ae28825f20d86751
siddhnigam-deloitte <svinayakamnigam@deloitte.com>
2022-05-30 12:14:01 +0530

    Added Encryption for password

--- src/main/java/com/cityguide/backend/jwt/WebSecurityConfig.java
+++ src/main/java/com/cityguide/backend/jwt/WebSecurityConfig.java
@@ -47,8 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();//NoOpPasswordEncoder.getInstance();
     }
 
