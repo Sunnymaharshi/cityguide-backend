bac2ad9813b25ba350f65cf1bacf40e3c118a1f8
Reddy_ Tangasani Maharshi (tanreddy@deloitte.com) <tanreddy@your.hostname.com>
2022-06-04 09:38:42 +0530

    code refractor

--- src/main/java/com/cityguide/backend/exceptions/GlobalErrorHandler.java
+++ src/main/java/com/cityguide/backend/exceptions/GlobalErrorHandler.java
@@ -9,26 +9,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalErrorHandler {
 
     @ExceptionHandler
-    public ResponseEntity<?> handleNotFound(NotFoundException notFoundException){
+    public ResponseEntity<?> handleNotFound(NotFoundException notFoundException) {
 
-        String response = notFoundException.getMessage() + "Not Found";
-        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+        String response = notFoundException.getMessage() + " Not Found";
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
 
     @ExceptionHandler
-    public ResponseEntity<?> handleUnAuthorized(UnAuthorisedException unAuthorisedException){
+    public ResponseEntity<?> handleUnAuthorized(UnAuthorisedException unAuthorisedException) {
 
         String response = "UNAUTHORIZED";
-        return new ResponseEntity<>(response,HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler
-    public ResponseEntity<?> handleUnknownError(Exception exception){
+    public ResponseEntity<?> handleUnknownError(Exception exception) {
 
 
-        String response = "Error: "+ exception.getMessage();
-        return new ResponseEntity<>(response,HttpStatus.FORBIDDEN);
+        String response = "Error: " + exception.getMessage();
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
 
     }
 }
