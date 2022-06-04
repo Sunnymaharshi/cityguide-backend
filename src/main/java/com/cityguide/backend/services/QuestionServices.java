6b2bd3017208615bce7d7dafaaad482ce5227018
Reddy_ Tangasani Maharshi (tanreddy@deloitte.com) <tanreddy@your.hostname.com>
2022-06-04 17:29:21 +0530

    added case ignore in get sim ques

--- src/main/java/com/cityguide/backend/services/QuestionServices.java
+++ src/main/java/com/cityguide/backend/services/QuestionServices.java
@@ -1,10 +1,9 @@
 package com.cityguide.backend.services;
 
-import com.cityguide.backend.response.mQuestion;
 import com.cityguide.backend.entities.Question;
-import com.cityguide.backend.entities.User;
 import com.cityguide.backend.jwt.JwtTokenUtil;
 import com.cityguide.backend.repositories.*;
+import com.cityguide.backend.response.mQuestion;
 import com.cityguide.backend.types.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -43,100 +42,92 @@ public class QuestionServices {
     @Autowired
     DownvoteRepository downvoteRepository;
 
-    public ResponseEntity<?> getAllQuestions(String city){
-        List<Question> questionList=cityRepository.findById(city).get().getQuestionList();
-        List<mQuestion> display=new ArrayList<>();
-        for (Question q:questionList)
-        {
-            display.add(new mQuestion(q.getQues_id(),q.getDescription(),q.getUsername(),q.getCity_name()));
+    public ResponseEntity<?> getAllQuestions(String city) {
+        List<Question> questionList = cityRepository.findById(city).get().getQuestionList();
+        List<mQuestion> display = new ArrayList<>();
+        for (Question q : questionList) {
+            display.add(new mQuestion(q.getQues_id(), q.getDescription(), q.getUsername(), q.getCity_name()));
         }
-        Collections.sort(display,new Comparator<mQuestion>(){
+        Collections.sort(display, new Comparator<mQuestion>() {
             @Override
-            public int compare(mQuestion q1,mQuestion q2)
-            {
+            public int compare(mQuestion q1, mQuestion q2) {
                 return Integer.compare(q2.getQues_id(), q1.getQues_id());
             }
         });
 
         return new ResponseEntity<>(display, HttpStatus.OK);
     }
+
     //get ques by id
-    public ResponseEntity<?> getQuestions(int id){
+    public ResponseEntity<?> getQuestions(int id) {
         try {
 
-            Question q=questionRepository.findById(id).get();
-            mQuestion display=new mQuestion(q.getQues_id(),q.getDescription(),q.getUsername(),q.getCity_name());
+            Question q = questionRepository.findById(id).get();
+            mQuestion display = new mQuestion(q.getQues_id(), q.getDescription(), q.getUsername(), q.getCity_name());
             return new ResponseEntity<>(display, HttpStatus.OK);
-        }
-        catch (Exception e)
-        {
-            return new ResponseEntity<>("Not Found",HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
         }
     }
 
     //posting question
-    public ResponseEntity<Question> postques(String requestTokenHeader, Question question)
-    {
-        String username=jwtTokenUtil.getUserFromToken(requestTokenHeader);
+    public ResponseEntity<Question> postques(String requestTokenHeader, Question question) {
+        String username = jwtTokenUtil.getUserFromToken(requestTokenHeader);
         question.setUsername(username);
-        return  new ResponseEntity<>(questionRepository.save(question),HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(questionRepository.save(question), HttpStatus.ACCEPTED);
     }
 
     //deleting question
-    public ResponseEntity<?> delques(String requestTokenHeader, int ques_id){
-        String username=jwtTokenUtil.getUserFromToken(requestTokenHeader);
-        Question user2= questionRepository.findById(ques_id).get();
-        if(username.equals(user2.getUsername())||isAdmin(username)){
-            try{
+    public ResponseEntity<?> delques(String requestTokenHeader, int ques_id) {
+        String username = jwtTokenUtil.getUserFromToken(requestTokenHeader);
+        Question user2 = questionRepository.findById(ques_id).get();
+        if (username.equals(user2.getUsername()) || isAdmin(username)) {
+            try {
                 questionRepository.deleteById(ques_id);
-                return  new  ResponseEntity<>("Question Deleted",HttpStatus.ACCEPTED);
+                return new ResponseEntity<>("Question Deleted", HttpStatus.ACCEPTED);
+            } catch (Exception e) {
+                return new ResponseEntity<>("Question Not Found", HttpStatus.NOT_FOUND);
             }
-            catch (Exception e){
-                return new ResponseEntity<>("Question Not Found",HttpStatus.NOT_FOUND);
-            }
-        }
-        else{
+        } else {
             return new ResponseEntity<>("Unauthorized!", HttpStatus.FORBIDDEN);
         }
     }
 
-    public ResponseEntity<?> getuserques(String requestTokenHeader)
-    {
-        String username=jwtTokenUtil.getUserFromToken(requestTokenHeader);
-        List<Question> questionList=userRepository.findById(username).get().getQuestionList();
-        List<mQuestion> display=new ArrayList<>();
-        for (Question q:questionList)
-        {
-            display.add(new mQuestion(q.getQues_id(),q.getDescription(),q.getUsername(),q.getCity_name()));
+    public ResponseEntity<?> getuserques(String requestTokenHeader) {
+        String username = jwtTokenUtil.getUserFromToken(requestTokenHeader);
+        List<Question> questionList = userRepository.findById(username).get().getQuestionList();
+        List<mQuestion> display = new ArrayList<>();
+        for (Question q : questionList) {
+            display.add(new mQuestion(q.getQues_id(), q.getDescription(), q.getUsername(), q.getCity_name()));
         }
-        return new ResponseEntity<>(display,HttpStatus.OK);
+        return new ResponseEntity<>(display, HttpStatus.OK);
     }
 
 
-    public ResponseEntity<?> getsimilarques(String query,String city)
-    {
-        List<Question> questionList=cityRepository.findById(city).get().getQuestionList();
-        List<mQuestion> displaylist=new ArrayList<>();
-        String words[]=query.split(" ");
-        for(Question question:questionList) {
+    public ResponseEntity<?> getsimilarques(String query, String city) {
+        List<Question> questionList = cityRepository.findById(city).get().getQuestionList();
+        List<mQuestion> displaylist = new ArrayList<>();
+        String words[] = query.split(" ");
+        for (Question question : questionList) {
             int hit = 0;
-            if (question.getDescription().contains(query)) {
-                displaylist.add(new mQuestion(question.getQues_id(),question.getDescription(),question.getUsername(),question.getCity_name()));
+            if (question.getDescription().toLowerCase().contains(query.toLowerCase())) {
+                displaylist.add(new mQuestion(question.getQues_id(), question.getDescription(), question.getUsername(), question.getCity_name()));
             } else {
                 for (String word : words) {
-                    if (question.getDescription().contains(word)) {
+                    if (question.getDescription().toLowerCase().contains(word.toLowerCase())) {
                         hit++;
                     }
                 }
                 if (hit > (words.length / 2)) {
-                    displaylist.add(new mQuestion(question.getQues_id(),question.getDescription(),question.getUsername(),question.getCity_name()));
+                    displaylist.add(new mQuestion(question.getQues_id(), question.getDescription(), question.getUsername(), question.getCity_name()));
                 }
             }
         }
-        return new ResponseEntity<>(displaylist,HttpStatus.OK);
+        return new ResponseEntity<>(displaylist, HttpStatus.OK);
     }
-    public Boolean isAdmin(String username){
-        String role=userRepository.findById(username).get().getRole();
+
+    public Boolean isAdmin(String username) {
+        String role = userRepository.findById(username).get().getRole();
         return role.equalsIgnoreCase(Role.ADMIN);
     }
 
